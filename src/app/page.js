81fc98fe09acea782a41de5da7ec5024965c1fcd
SteveFau <stepan.fau@gmail.com>
2025-11-27 20:29:15 +0100

    basic UI mockup

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,38 @@
 
+'use client'
+
+import { useState } from 'react';
+
 export default function Home() {
+
+  const [inputFields, setInputFields] = useState([1, 2, 3]);
+
+  function handleAdd() {
+
+    const newInputField = crypto.randomUUID();
+
+    setInputFields([...inputFields, newInputField]);
+  }
+
   return (
-    <></>
+    <>
+      <div className="input-section">
+        <form onSubmit={(e) => e.preventDefault()}>
+          
+          {inputFields.map(field => {
+            return(
+              <div key={field}>
+                <input />
+                <input />
+                <button>Delete</button>
+              </div>
+            );
+          })}
+          
+          <button onClick={handleAdd}>Add Fields</button>
+        </form>
+      </div>
+      <div className="output-section"></div>
+    </>
   );
 }
