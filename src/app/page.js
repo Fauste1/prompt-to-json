237865cc23330e5f607a12e7c00d4a16b170156d
SteveFau <stepan.fau@gmail.com>
2025-11-28 17:31:08 +0100

    styling cleanup

--- src/app/page.js
+++ src/app/page.js
@@ -48,50 +48,53 @@ export default function Home() {
   }
 
   return (
-    <div style={myStyle}>
-      <div className="input-section section" >
-        <h1>Your Input</h1>
-        <form onSubmit={(e) => e.preventDefault()}>
-          
-          {inputFields.map(field => {
-            return(
-              <div key={field.id}>
-                <input 
-                  value={field.keyInputValue}
-                  onChange={e => handleFieldUpdate(field.id, e.target.value, field.valueInputValue)}
-                />
-                <input 
-                  value={field.valueInputValue}
-                  onChange={e => handleFieldUpdate(field.id, field.keyInputValue, e.target.value)}
-                />
-                <button onClick={() => {
-                  setInputFields(
-                    inputFields.filter(f => f.id !== field.id)
-                  )
-                }}>Delete</button>
-              </div>
-            );
-          })}
-          
-          <button onClick={handleAdd}>Add Fields</button>
-        </form>
-      </div>
-      <div className="output-section section">
-          <h1>Your JSON output</h1>
-          <div style={{background: '#fff'}}>
-            &#123; 
-            <div style={{marginLeft: '10px'}}>
-              { inputFields.map(field => {
-                return(
-                  <div key={field.id}>
-                  {field.keyInputValue && <span>&quot;{field.keyInputValue}&quot;</span>}{field.keyInputValue && field.valueInputValue && <span>:</span>} {field.valueInputValue && <span>&quot;{field.valueInputValue}&quot;</span>}{field.valueInputValue || field.keyInputValue ? <span>,</span> : ''}
+    <>
+      <div style={myStyle}>
+        <div className="input-section section" >
+          <h1>Your Input</h1>
+          <form onSubmit={(e) => e.preventDefault()}>
+            
+            {inputFields.map(field => {
+              return(
+                <div key={field.id}>
+                  <input 
+                    value={field.keyInputValue}
+                    onChange={e => handleFieldUpdate(field.id, e.target.value, field.valueInputValue)}
+                  />
+                  <input 
+                    value={field.valueInputValue}
+                    onChange={e => handleFieldUpdate(field.id, field.keyInputValue, e.target.value)}
+                  />
+                  <button onClick={() => {
+                    setInputFields(
+                      inputFields.filter(f => f.id !== field.id)
+                    )
+                  }}>Delete</button>
                 </div>
-                );
-              }) }
+              );
+            })}
+            
+            <button onClick={handleAdd}>Add Fields</button>
+          </form>
+        </div>
+        <div className="output-section section">
+            <h1>Your JSON output</h1>
+            <div style={{background: '#fff'}}>
+              &#123; 
+              <div style={{marginLeft: '10px'}}>
+                { inputFields.map(field => {
+                  return(
+                    <div key={field.id}>
+                    {field.keyInputValue && <span>&quot;{field.keyInputValue}&quot;</span>}{field.keyInputValue && field.valueInputValue && <span>:</span>} {field.valueInputValue && <span>&quot;{field.valueInputValue}&quot;</span>}{field.valueInputValue || field.keyInputValue ? <span>,</span> : ''}
+                  </div>
+                  );
+                }) }
+              </div>
+              &#125;
             </div>
-            &#125;
-          </div>
+        </div>
       </div>
-    </div>
+      <p style={{ margin: '50px 0px 0px 50px' }}>Built by <a href="https://github.com/Fauste1" target="_blank">Štěpán (Steve) Fau</a></p>
+    </>
   );
 }
